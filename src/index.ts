a990a378e422d8cf369b875522b548218b7743d1
kyleboyer <kyleboyer@comcast.net>
2025-06-22 21:46:20 -0500

    Added demoLightModes func

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import { updateLightingMode, updateTime } from "./device";
 import { LightingModes } from "./light-modes";
+import { sleep } from "./utils";
+
+const demoLightingModes = async (secondsToDemo = 5) => {
+    for(const [modeName, modeNumber] of Object.entries(LightingModes)){
+        console.log(`Changing to mode: ${modeName}`);
+        await updateLightingMode(modeNumber);
+        await sleep(secondsToDemo * 1000);
+    }
+}
 
 const main = async () => {
     await updateTime();
-    await updateLightingMode(LightingModes.Glittering);
+    await updateLightingMode(LightingModes.Launch);
 }
 main();
